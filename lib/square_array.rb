ee988613daed3ec9c90030111d8a94a7b82e3f4c
gregarious-number-3828 <muse.malize@gmail.com>
2019-08-26 15:13:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   new_array = []
   
-  while array[counter] do
+  while array[counter] < array.length do
     new_element = array[counter]*array[counter]
     new_array << new_element
   end
